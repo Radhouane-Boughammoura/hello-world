3b3f61e8b7689f3de218cc05bbd6e59765f411a7
Radhouane-Boughammoura <98164370+Radhouane-Boughammoura@users.noreply.github.com>
2022-01-22 19:21:53 +0100

    Add files via upload
    this is a quadtree painting

---
+++ QuadTreeDisplay.java
@@ -0,0 +1,49 @@
+package radhouanepackage;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.applet.*;
+
+import radhouanepackage.MyQuadTree.Node;
+
+public class QuadTreeDisplay extends Applet {
+
+	private MyQuadTree<Integer, String> qtree = new MyQuadTree<Integer, String>(); 
+		
+	public void paint(Graphics g) {
+        //renderQuadTree(g, );
+    }
+    private void render(Graphics g) {
+        setBackground(Color.white);        
+        System.out.println("-----------");
+        g.drawRect(10, 10, 20, 20);
+    }
+    /** Draw the quadtree root on the applet
+     *  it subdividing the enclosing rectangle  
+     */
+    public void query2D(Interval2D<Integer> rect) {
+        query2D(qtree.root, rect);
+    }
+    /**@param node MyQuadtree.Node to represent
+    *  @param x top left of rectangle
+    *  @param y top right of rectangle
+    *  @param width width of rectangle
+    *  @param height height of rectangle
+    *  
+    */
+    private void query2D(Node h, Interval2D<Integer> rect) {
+        if (h == null) return;
+        Integer xmin = rect.intervalX.min();
+        Integer ymin = rect.intervalY.min();
+        Integer xmax = rect.intervalX.max();
+        Integer ymax = rect.intervalY.max();
+        if (rect.contains(h.x, h.y)
+            System.out.println("    (" + h.x + ", " + h.y + ") " + h.value);
+        if ( less(xmin, h.x) &&  less(ymin, h.y)) query2D(h.SW, rect);
+        if ( less(xmin, h.x) && !less(ymax, h.y)) query2D(h.NW, rect);
+        if (!less(xmax, h.x) &&  less(ymin, h.y)) query2D(h.SE, rect);
+        if (!less(xmax, h.x) && !less(ymax, h.y)) query2D(h.NE, rect);
+    }
+    
+}
